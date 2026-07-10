f6347f6eb77555cacd6dd9681ce44f82ae4a6088
Your name <your@email.com>
2026-07-10 14:15:59 +0530

    Resume Uploading

--- api/routes.py
+++ api/routes.py
@@ -1,4 +1,7 @@
 from fastapi import APIRouter
+from fastapi import UploadFile, File
+import shutil
+import uuid
 from pydantic import BaseModel
 
 from services.gemini_services import ask_gemini
@@ -26,3 +29,22 @@ def health():
 def ask(request: PromptRequest):
     answer = ask_gemini(request.prompt)
     return answer
+
+@router.post("/resume/upload")
+def upload_resume(file: UploadFile = File(...)):
+
+    if not file.filename.lower().endswith(".pdf"):
+        return {
+            "error": "Only PDF files are allowed."
+        }
+    
+    unique_filename = f"{uuid.uuid4()}.pdf"
+    file_path = f"uploads/{unique_filename}"
+
+    with open(file_path, "wb") as buffer:
+        shutil.copyfileobj(file.file, buffer)
+
+    return {
+        "message": "Resume uploaded successfully.",
+        "filename": file.filename
+    }
